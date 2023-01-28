376500cd54e1bfe629edf6d3cc31edc19226088a
Andrés Mauricio <andresuqui2@gmail.com>
2023-01-28 20:35:26 +0100

    sass cambios

--- Desarrollo_Web_en_entorno_cliente/Casa/Trabajos/Unit1 - JS/Exercise-week4/src/constants.ts
+++
@@ -1,3 +0,0 @@
-// export const SERVER = 'http://localhost:3000';
-export const SERVER = "http://arturober.com:5007";
-export const RESTAURANTS = "http://arturober.com:5007/restaurants";
--- Desarrollo_Web_en_entorno_cliente/Casa/Trabajos/Unit1 - JS/Exercise-week4/src/interfaces/restaurant.ts
+++
@@ -1,9 +0,0 @@
-export interface Restaurant {
-    id?: number;
-    image: string;
-    name: string;
-    description: string;
-    daysOpen: string[];
-    phone: string;
-    cuisine: string;
-}
